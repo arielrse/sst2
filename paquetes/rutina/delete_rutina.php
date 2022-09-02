14664e00655807870b8d793753d32a2bbe4017f4
borboton13 <borboton23@gmail.com>
2022-09-01 23:47:58 -0400

    correccion eliminar rutina

--- paquetes/rutina/delete_rutina.php
+++ paquetes/rutina/delete_rutina.php
@@ -7,7 +7,7 @@ require("../../funciones/motor.php");
 	$idrutina = $_POST['idrutina'];
 	$codForm  = $_POST['codForm'];
 
-	$codForm = sprintf("%'.03d\n", $codForm);
+	//$codForm = sprintf("%'.03d\n", $codForm);
 
     mysqli_query($conexion, "delete from rutina where idrutina = $idrutina");
     mysqli_query($conexion, "delete from rutina".$codForm." where idrutina = $idrutina");
