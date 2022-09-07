86fe5b692298e38f3e70c8d5cd5a3443cb4b0b8f
borboton13 <borboton23@gmail.com>
2022-09-07 11:38:13 -0400

    config tables num

--- modulos/seguimiento_tecnico/rutina001/plantilla.php
+++ modulos/seguimiento_tecnico/rutina001/plantilla.php
@@ -70,6 +70,53 @@ function getPlantilla($conexion, $jsonData, $idgrupo){
     $f09_2 = $f_verificacion[8]->f09_2 ? $check : $uncheck;
     $f09_3 = $f_verificacion[8]->f09_3;
 
+    /** g_desarrollo **/
+    $g_desarrollo = $obj->{'g_desarrollo'};
+    $g01_1 = $g_desarrollo[0]->g01_1 ? $check : $uncheck;
+    $g01_2 = $g_desarrollo[0]->g01_2 ? $check : $uncheck;
+    $g01_3 = $g_desarrollo[0]->g01_3;
+    $g02_1 = $g_desarrollo[1]->g02_1 ? $check : $uncheck;
+    $g02_2 = $g_desarrollo[1]->g02_2 ? $check : $uncheck;
+    $g02_3 = $g_desarrollo[1]->g02_3;
+    $g03_1 = $g_desarrollo[2]->g03_1 ? $check : $uncheck;
+    $g03_2 = $g_desarrollo[2]->g03_2 ? $check : $uncheck;
+    $g03_3 = $g_desarrollo[2]->g03_3;
+    $g04_1 = $g_desarrollo[3]->g04_1 ? $check : $uncheck;
+    $g04_2 = $g_desarrollo[3]->g04_2 ? $check : $uncheck;
+    $g04_3 = $g_desarrollo[3]->g04_3;
+    $g05_1 = $g_desarrollo[4]->g05_1 ? $check : $uncheck;
+    $g05_2 = $g_desarrollo[4]->g05_2 ? $check : $uncheck;
+    $g05_3 = $g_desarrollo[4]->g05_3;
+    $g06_1 = $g_desarrollo[5]->g06_1 ? $check : $uncheck;
+    $g06_2 = $g_desarrollo[5]->g06_2 ? $check : $uncheck;
+    $g06_3 = $g_desarrollo[5]->g06_3;
+    $g07_1 = $g_desarrollo[6]->g07_1 ? $check : $uncheck;
+    $g07_2 = $g_desarrollo[6]->g07_2 ? $check : $uncheck;
+    $g07_3 = $g_desarrollo[6]->g07_3;
+    $g08_1 = $g_desarrollo[7]->g08_1 ? $check : $uncheck;
+    $g08_2 = $g_desarrollo[7]->g08_2 ? $check : $uncheck;
+    $g08_3 = $g_desarrollo[7]->g08_3;
+    $g09_1 = $g_desarrollo[8]->g09_1;
+    $g09_2 = $g_desarrollo[8]->g09_2;
+    $g10_1 = $g_desarrollo[9]->g10_1;
+    $g10_2 = $g_desarrollo[9]->g10_2;
+    $g11_1 = $g_desarrollo[10]->g11_1;
+    $g11_2 = $g_desarrollo[10]->g11_2;
+    $g12_1 = $g_desarrollo[11]->g12_1;
+    $g12_2 = $g_desarrollo[11]->g12_2;
+    $g13_1 = $g_desarrollo[12]->g13_1;
+    $g13_2 = $g_desarrollo[12]->g13_2;
+    $g14_1 = $g_desarrollo[13]->g14_1;
+    $g14_2 = $g_desarrollo[13]->g14_2;
+    $g15_1 = $g_desarrollo[14]->g15_1;
+    $g15_2 = $g_desarrollo[14]->g15_2;
+    $g16_1 = $g_desarrollo[15]->g16_1;
+    $g16_2 = $g_desarrollo[15]->g16_2;
+    $g17_1 = $g_desarrollo[16]->g17_1;
+    $g17_2 = $g_desarrollo[16]->g17_2;
+    $g18_1 = $g_desarrollo[17]->g18_1;
+    $g18_2 = $g_desarrollo[17]->g18_2;
+
     $plantilla =
 '<body>    
     <div class="card-">
@@ -304,64 +351,203 @@ function getPlantilla($conexion, $jsonData, $idgrupo){
                         <tbody>
                             <tr>
                                 <td class="col-40p">'.constant('G01').'</td>
-                                <td class="col-10p">'.$f01_1.' Si</td>
-                                <td class="col-10p">'.$f01_2.' No</td>
-                                <td class="col-40p">Obs. '.$f01_3.'</td>
+                                <td class="col-10p">'.$g01_1.' Si</td>
+                                <td class="col-10p">'.$g01_2.' No</td>
+                                <td class="col-40p">Obs. '.$g01_3.'</td>
                             </tr>
                             <tr>
                                 <td class="col-40p">'.constant('G02').'</td>
-                                <td class="col-10p">'.$f02_1.' Si</td>
-                                <td class="col-10p">'.$f02_2.' No</td>
-                                <td class="col-40p">Obs. '.$f02_3.'</td>
+                                <td class="col-10p">'.$g02_1.' Si</td>
+                                <td class="col-10p">'.$g02_2.' No</td>
+                                <td class="col-40p">Obs. '.$g02_3.'</td>
                             </tr>
                             <tr>
                                 <td class="col-40p">'.constant('G03').'</td>
-                                <td class="col-10p">'.$f03_1.' Si</td>
-                                <td class="col-10p">'.$f03_2.' No</td>
-                                <td class="col-40p">Obs. '.$f03_3.'</td>
+                                <td class="col-10p">'.$g03_1.' Si</td>
+                                <td class="col-10p">'.$g03_2.' No</td>
+                                <td class="col-40p">Obs. '.$g03_3.'</td>
                             </tr>
                             <tr>
                                 <td class="col-40p">'.constant('G04').'</td>
-                                <td class="col-10p">'.$f04_1.' Si</td>
-                                <td class="col-10p">'.$f04_2.' No</td>
-                                <td class="col-40p">Obs. '.$f04_3.'</td>
+                                <td class="col-10p">'.$g04_1.' Si</td>
+                                <td class="col-10p">'.$g04_2.' No</td>
+                                <td class="col-40p">Obs. '.$g04_3.'</td>
                             </tr>
                             <tr>
                                 <td class="col-40p">'.constant('G05').'</td>
-                                <td class="col-10p">'.$f05_1.' Si</td>
-                                <td class="col-10p">'.$f05_2.' No</td>
-                                <td class="col-40p">Obs. '.$f05_3.'</td>
+                                <td class="col-10p">'.$g05_1.' Si</td>
+                                <td class="col-10p">'.$g05_2.' No</td>
+                                <td class="col-40p">Obs. '.$g05_3.'</td>
                             </tr>
                             <tr>
                                 <td class="col-40p">'.constant('G06').'</td>
-                                <td class="col-10p">'.$f06_1.' Si</td>
-                                <td class="col-10p">'.$f06_2.' No</td>
-                                <td class="col-40p">Obs. '.$f06_3.'</td>
+                                <td class="col-10p">'.$g06_1.' Si</td>
+                                <td class="col-10p">'.$g06_2.' No</td>
+                                <td class="col-40p">Obs. '.$g06_3.'</td>
                             </tr>
                             <tr>
                                 <td class="col-40p">'.constant('G07').'</td>
-                                <td class="col-10p">'.$f07_1.' Si</td>
-                                <td class="col-10p">'.$f07_2.' No</td>
-                                <td class="col-40p">Obs. '.$f07_3.'</td>
+                                <td class="col-10p">'.$g07_1.' Si</td>
+                                <td class="col-10p">'.$g07_2.' No</td>
+                                <td class="col-40p">Obs. '.$g07_3.'</td>
                             </tr>
                             <tr>
                                 <td class="col-40p">'.constant('G08').'</td>
-                                <td class="col-10p">'.$f08_1.' Si</td>
-                                <td class="col-10p">'.$f08_2.' No</td>
-                                <td class="col-40p">Obs. '.$f08_3.'</td>
+                                <td class="col-10p">'.$g08_1.' Si</td>
+                                <td class="col-10p">'.$g08_2.' No</td>
+                                <td class="col-40p">Obs. '.$g08_3.'</td>
                             </tr>
                             <tr>
                                 <td class="col-40p">'.constant('G09').'</td>
-                                <td class="col-10p">'.$f09_1.' Si</td>
-                                <td class="col-10p">'.$f09_2.' No</td>
-                                <td class="col-40p">Obs. '.$f09_3.'</td>
-                            </tr>
-                            
-                                                    
+                                <td class="col-10p num">'.$g09_1.'</td>
+                                <td class="col-10p"></td>
+                                <td class="col-40p">Obs. '.$g09_2.'</td>
+                            </tr>                        
                         </tbody>        
                     </table>									                        
                 </main>
-                    
+                
+                <main>
+                
+                </main>
+                    <table>
+                        <tr>
+                            <td>
+                                <table>
+                                    <tr>
+                                        <td>
+                                            <table class="tborder">
+                                                <tr>
+                                                    <td>N° Bat.</td>
+                                                    <td>VDC</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B1</td>
+                                                    <td></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B2</td>
+                                                    <td></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B3</td>
+                                                    <td></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B4</td>
+                                                    <td></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B5</td>
+                                                    <td></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B6</td>
+                                                    <td></td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                        <td>
+                                            <table class="tborder">
+                                                <tr>
+                                                    <td>N° Bat.</td>
+                                                    <td>VDC</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B7</td>
+                                                    <td></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B8</td>
+                                                    <td></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B9</td>
+                                                    <td></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B10</td>
+                                                    <td></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B11</td>
+                                                    <td></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B12</td>
+                                                    <td></td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                        <td>
+                                            <table class="tborder">
+                                                <tr>
+                                                    <td>N° Bat.</td>
+                                                    <td>VDC</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B13</td>
+                                                    <td></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B14</td>
+                                                    <td></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B15</td>
+                                                    <td></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B16</td>
+                                                    <td></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B17</td>
+                                                    <td></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B18</td>
+                                                    <td></td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                        <td>
+                                            <table class="tborder">
+                                                <tr>
+                                                    <td>N° Bat.</td>
+                                                    <td>VDC</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B19</td>
+                                                    <td></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B20</td>
+                                                    <td></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B21</td>
+                                                    <td></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B22</td>
+                                                    <td></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B23</td>
+                                                    <td></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BB2-B24</td>
+                                                    <td></td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                </table>    
+                            </td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </div>
