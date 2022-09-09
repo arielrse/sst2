72fa095aa70ec4fa14d74afe0af670d8c5949179
borboton13 <borboton23@gmail.com>
2022-09-09 12:00:28 -0400

    caecera, footer

--- modulos/seguimiento_tecnico/rutina001/plantilla.php
+++ modulos/seguimiento_tecnico/rutina001/plantilla.php
@@ -9,36 +9,12 @@ function getPlantilla($conexion, $jsonData, $idgrupo){
     $check   = "<img style='vertical-align:middle' src='../../../img/checked.png'>";
     $uncheck = "<img style='vertical-align:middle' src='../../../img/unchecked.png'>";
 
-    $cm                 = $obj->{'cm'};
-    $sitioId            = $obj->{'sitioId'};
-    $propertyId         = $obj->{'propertyId'};
-    $b_idenActivo       = $obj->{'b_idenActivo'};
-    $b_nroActivo        = $obj->{'b_nroActivo'};
+    $titulo = 'RUTINA DE MANTENIMIENTO PREVENTIVO - BANCO DE BATERÍAS';
+    $cabecera        = getCabecera($conexion, $jsonData, $idgrupo, $titulo, true);
+    $subPlantilla1   = getSubPlantilla1($jsonData);
+    $subPlantilla2   = getSubPlantilla2($jsonData);
+    $footerPlantilla = getFooter($jsonData);
 
-    $b1_noAplica    = $obj->{'b1_noAplica'}     ? $check : $uncheck;
-    $b1_idenActivo  = $obj->{'b1_idenActivo'}   ? $obj->{'b1_idenActivo'}  : "" ;
-    $b1_nroActivo   = $obj->{'b1_nroActivo'}    ? $obj->{'b1_nroActivo'}   : "" ;
-
-    $c_fechaRealizacion = dateToLiteral($obj->{'c_fechaRealizacion'});
-
-    $d_horainicio   = $obj->{'d_horainicio'};
-    $d_horafin      = $obj->{'d_horafin'};
-    $tiempoTrans    = timeDiff($d_horainicio, $d_horafin);
-
-    //---------------------------
-    $dataPers = getPersonalOyM($conexion, $obj->{'e_personal'});
-    $nombre1 = $dataPers['nombre'];
-    $cargo1  = $dataPers['cargo'];
-    $cel1    = $dataPers['cel'];
-
-    $dataPersMtto = getPersonalMtto($conexion, $idgrupo);
-    $nombre2 = $dataPersMtto['nombre2'];
-    $nombre3 = $dataPersMtto['nombre3'];
-    $cargo2  = $dataPersMtto['cargo2'];
-    $cargo3  = $dataPersMtto['cargo3'];
-    $cel2    = $dataPersMtto['cel2'];
-    $cel3    = $dataPersMtto['cel3'];
-    //---------------------------
 
     /** f_verificacion **/
     $f_verificacion = $obj->{'f_verificacion'};
@@ -152,169 +128,15 @@ function getPlantilla($conexion, $jsonData, $idgrupo){
     $g20_2_2 = $g_descarga->g20_2_2;
     $g20_2_3 = $g_descarga->g20_2_3;
 
-
-
-    $subPlantilla1 = getSubPlantilla1($jsonData);
-    $subPlantilla2 = getSubPlantilla2($jsonData);
-    $footerPlantilla = getFooter($jsonData);
-
     $plantilla =
 '<body>
     <div class="card-">
         <div class="card-body-">
             <div id="invoice">
                 <div class="invoice">
-                <table>
-                    <tr>
-                        <th class="col-2">
-                            <div><img src="../../../img/logo-entel.png" width="90" alt="" /></div>
-                        </th>
-                        <th class="col-10 company-details">
-                            <div>
-                                <div>RMP-BB-001/2021</div>
-                                <div>Subgerencia de Operación y Mantenimiento</div>
-                                <div>Sistemas de Energía y Climatización</div>
-                            </div>
-                        </th>
-                    </tr>
-                    <tr>
-                        <th class="text-center" colspan="2">
-                            <h4>RUTINA DE MANTENIMIENTO PREVENTIVO - BANCO DE BATERÍAS</h4>
-                        </th>
-                    </tr>
-                </table>
-                <main>
-                    <div class="notices">
-                        <div class="notice"><strong>A. Identificación del Sitio</strong></div>
-                    </div>
-                </main>																  
-                <main>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td class="col-10p no">CM/SCM:</td>
-                                <td class="col-20p">'. $cm .'</td>
-                                <td class="col-10p no">ID Sitio:</td>
-                                <td class="col-10p">'. $sitioId .'</td>
-                                <td class="col-12p no">Property_id:</td>
-                                <td class="col-40p">'. $propertyId .'</td>
-                            </tr>     
-                        </tbody>        
-                    </table>									                        
-                </main>
-               
-                <main>
-                    <div class="notices">
-                        <div class="notice"><strong>B. Identificación del Activo</strong></div>
-                    </div>
-                </main>
-                <main>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td class="col-15p no">Identificación:</td>
-                                <td>'. $b_idenActivo .'</td>
-                                <td class="col-15p no">N° Activo Fijo:</td>
-                                <td>'. $b_nroActivo .'</td>
-                            </tr>     
-                        </tbody>        
-                    </table>									                        
-                </main>
                 
-                <main>
-                    <div class="notices">
-                        <div class="notice"><strong>B1. Identificación del Activo Madre</strong></div>
-                    </div>
-                </main>																  
-                <main>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td class="col-10p no">NO APLICA</td>
-                                <td class="col-5p" align="center">'.$b1_noAplica.'</td>
-                                <td class="col-13p no">Identificación:</td>
-                                <td>'.$b1_idenActivo.'</td>
-                                <td class="col-13p no">N° Activo Fijo:</td>
-                                <td>'.$b1_nroActivo.'</td>
-                            </tr>     
-                        </tbody>        
-                    </table>									                        
-                </main>
-                
-                <main>
-                    <div class="notices">
-                        <div class="notice"><strong>C. Fecha de Realización</strong></div>
-                    </div>
-                </main>																  
-                <main>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td class="col-30p no">Fecha de mantenimiento:</td>
-                                <td class="col-70p">'.$c_fechaRealizacion.'</td>
-                            </tr>     
-                        </tbody>        
-                    </table>									                        
-                </main>
-                
-                <main>
-                    <div class="notices">
-                        <div class="notice"><strong>D. Tiempo de Duración</strong></div>
-                    </div>
-                </main>																  
-                <main>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td class="col-15p no">Hora de inicio:</td>
-                                <td>'.$d_horainicio.'</td>
-                                <td class="col-20p no">Hora de Culminación:</td>
-                                <td>'.$d_horafin.'</td>
-                                <td class="col-20p no">Tiempo Transcurrido:</td>
-                                <td>'.$tiempoTrans.'</td>
-                            </tr>     
-                        </tbody>        
-                    </table>									                        
-                </main>
-                
-                <main>
-                    <div class="notices">
-                        <div class="notice"><strong>E. Personal Técnico Responsable</strong></div>
-                    </div>
-                </main>	
-                <main>
-                    <table class="tborder">
-                        <tbody> 
-                            <tr>
-                                <td class="col-8p no">Nombre:</td>
-                                <td class="col-30p">'.$nombre1.'</td>
-                                <td class="col-7p no">Cargo:</td>
-                                <td class="col-35p">'.$cargo1.'</td>
-                                <td class="col-10p no">Teléfono:</td>
-                                <td class="col-8p">'.$cel1.'</td>
-                            </tr>
-                            <tr>
-                                <td class="col-8p no">Nombre:</td>
-                                <td class="col-30p">'.$nombre2.'</td>
-                                <td class="col-7p no">Cargo:</td>
-                                <td class="col-35p">'.$cargo2.'</td>
-                                <td class="col-10p no">Teléfono:</td>
-                                <td class="col-8p">'.$cel2.'</td>
-                            </tr>
-                            <tr>
-                                <td class="col-8p no">Nombre:</td>
-                                <td class="col-30p">'.$nombre3.'</td>
-                                <td class="col-7p no">Cargo:</td>
-                                <td class="col-35p">'.$cargo3.'</td>
-                                <td class="col-10p no">Teléfono:</td>
-                                <td class="col-8p">'.$cel3.'</td>
-                            </tr>
-                            
-                            
-                        </tbody>        
-                    </table>									                        
-                </main>
-                
+                '. $cabecera .'
+                                                
                 <main>
                     <div class="notices">
                         <div class="notice"><strong>F. Verificación Visual - Estado del Activo</strong></div>
@@ -1398,45 +1220,6 @@ function getSubPlantilla2($jsonData){
     </main>';
 
     return $plantilla;
-}
-
-function getFooter($jsonData){
-
-    $obj = json_decode($jsonData);
-    $h_pendientes       = $obj->{'h_pendientes'};
-    $i_recomendaciones  = $obj->{'i_recomendaciones'};
-    $j_observaciones    = $obj->{'j_observaciones'};
-
-    $planilla = '
-    <main>
-        <div class="notices">
-            <div class="notice"><strong>H. Pendientes</strong></div>
-        </div>
-    </main>
-    <main>
-        <div class="areatext">'. $h_pendientes .'</div>
-    </main>
-    
-    <main>
-        <div class="notices">
-            <div class="notice"><strong>I. Recomendaciones</strong></div>
-        </div>
-    </main>
-    <main>
-        <div class="areatext">'. $i_recomendaciones .'</div>
-    </main>
-    
-    <main>
-        <div class="notices">
-            <div class="notice"><strong>J. Observaciones</strong></div>
-        </div>
-    </main>
-    <main>
-        <div class="areatext">'. $j_observaciones .'</div>
-    </main>
-    ';
-
-    return $planilla;
 }
 
 ?>
