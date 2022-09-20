81a1d47b77dd4a83c5270d4cb58a5f7c2b7510d9
AS <arielrse@gmail.com>
2022-09-20 10:07:15 -0400

    cambios index

--- index.php
+++ index.php
@@ -68,15 +68,7 @@ $sw = isset($_GET["sw"]) ? $_GET["sw"] : 0;
                                     </p>
                                 </div>
 
-                                <div class="d-grid">
-                                    <!--<a class="btn my-4 shadow-sm btn-white" href="javascript:;">
-                                        <span class="d-flex justify-content-center align-items-center">
-                                            <img class="me-2" src="assets/images/icons/search.svg" width="16" alt="Image Description">
-                                            <span>Sign in with Google</span>
-										</span>
-                                    </a>-->
-                                    <!--<a href="javascript:;" class="btn btn-facebook"><i class="bx bxl-facebook"></i>Sign in with Facebook</a>-->
-                                </div>
+                                <div class="d-grid"></div>
 
                                 <div class="login-separater text-center mb-4"> <!--<span>INICIAR SESIÓN</span>-->
                                     <hr/>
