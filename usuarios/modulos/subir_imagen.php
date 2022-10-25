786fd45ead45194fe2a74384482bf9b5e56db184
borboton13 <borboton23@gmail.com>
2022-10-25 11:20:06 -0400

    subir imagen rutina mejora subir doc evento

--- usuarios/modulos/subir_imagen.php
+++ usuarios/modulos/subir_imagen.php
@@ -4,11 +4,12 @@ require("../../funciones/ImageUtils.php");
 
     $idrutina = $_POST['idrutina'];
     $titulo = $_POST['titulo'];
+    $coddep = $_POST['coddep'];
 
     $ruta_carpeta = "../../fotos/";
     $imagen = $_FILES['imagen']['name'];
 
-    $nombre_archivo = "IMG_".date("dHis") . "." . pathinfo($imagen, PATHINFO_EXTENSION);
+    $nombre_archivo = "IMG_".$coddep."_".$idrutina."_".date("dHis") . "." . pathinfo($imagen, PATHINFO_EXTENSION);
     $ruta_guardar_archivo = $ruta_carpeta . $nombre_archivo;
 
 
