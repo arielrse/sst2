a30d3e789f28d8eb757d2a300576ae70bc11d757
AS <arielrse@gmail.com>
2022-11-11 22:48:55 -0400

    reporte 003 catastro

--- modulos/seguimiento_tecnico/ReporteCatastroUtils.php
+++ modulos/seguimiento_tecnico/ReporteCatastroUtils.php
@@ -247,6 +247,117 @@ function getCabecera002($conexion, $jsonData, $idgrupo, $idevento){
     ';
     return $plantilla;
 }
+function getCabecera003($conexion, $jsonData, $idgrupo, $idevento){
+
+    $resIden = mysqli_query($conexion, "SELECT e.`idevento`, e.`inicio`, s.`nombre`, s.`codsitio`, s.`tiponodo`, c.`nombre` AS nombreCentro, c.`coddep`,
+                                                d.`nombre` AS departamento, s.`provincia`, s.`localidad`
+                                                FROM evento e 
+                                                LEFT JOIN sitio s 	 ON e.`idsitio` = s.`idsitio`
+                                                LEFT JOIN centro c 	 ON s.`idcentro` = c.`idcentro`
+                                                LEFT JOIN departamento d ON c.`iddepartamento` = d.`iddepartamento`
+                                                WHERE e.`idevento` = ".$idevento);
+    $dataIden = mysqli_fetch_array($resIden);
+    $departamento   = $dataIden['departamento'];
+    $provincia      = $dataIden['provincia'];
+    $localidad      = $dataIden['localidad'];
+
+
+    $obj = json_decode($jsonData);
+    $check   = "<img style='vertical-align:middle' src='../../../img/checked.png'>";
+    $uncheck = "<img style='vertical-align:middle' src='../../../img/unchecked.png'>";
+
+    $cm                 = $obj->{'cm'};
+    $sitioId            = $obj->{'sitioId'};
+    $propertyId         = $obj->{'propertyId'};
+    $cod_activo         = $obj->{'cod_activo'};
+    $cod_fijo           = $obj->{'cod_fijo'};
+    $c_fechaRealizacion = dateToLiteral($obj->{'c_fechaRealizacion'});
+    $indoor_outdoor     = $obj->{'indoor_outdoor'};
+
+    //$d_horainicio   = $obj->{'d_horainicio'};
+    //$d_horafin      = $obj->{'d_horafin'};
+    // $tiempoTrans    = timeDiff($d_horainicio, $d_horafin);
+
+
+
+    $dataPersMtto = getPersonalMtto($conexion, $idgrupo);
+    $nombre2 = $dataPersMtto['nombre2'];
+    $nombre3 = $dataPersMtto['nombre3'];
+    //$cargo2  = $dataPersMtto['cargo2'];
+    //$cargo3  = $dataPersMtto['cargo3'];
+    //$cel2    = $dataPersMtto['cel2'];
+    //$cel3    = $dataPersMtto['cel3'];
+    //---------------------------
+
+
+    $plantilla = '
+    <table>
+        <tr>
+            <th class="col-2">
+                <div><img src="../../../img/logo-entel.png" width="90" alt="" /></div>
+            </th>
+            <th class="col-10 company-details">
+                <div>
+                    <div><b>FORMULARIO DE CATASTRO SISTEMAS DE ENERGÍA</b></div>
+                    <div><b>GRUPO ELECTRÓGENO (GE)</b></div>
+                   
+                </div>
+            </th>
+        </tr>
+        <tr>
+            <th class="text-center" colspan="2">
+               
+            </th>
+        </tr>
+    </table>
+   												  
+    <main>
+        <table class="tborder">
+            <tbody>
+                <tr>
+                    <td class="col-25p no">CM/SCM:</td>
+                    <td class="col-25p">'. $cm .'</td>
+                    <td class="col-25p no">ID Sitio:</td>
+                    <td class="col-25p">'. $sitioId .'</td>
+                </tr>
+                <tr>
+                    <td class="col-25p no">Nombre:</td>
+                    <td class="col-25p">'.$nombre2.'</td>
+                    <td class="col-25p no">DEPARTAMENTO:</td>
+                    <td class="col-25p">'. $departamento .'</td>
+                </tr>  
+                 <tr>
+                    <td class="col-25p no">Nombre:</td>
+                    <td class="col-30p">'.$nombre3.'</td>
+                    <td class="col-25p no">PROVINCIA:</td>
+                    <td class="col-25p">'. $provincia .'</td>
+                </tr>  
+                <tr>
+                    <td class="col-30p no">Fecha de catastro:</td>
+                    <td class="col-25p">'.$c_fechaRealizacion.'</td>
+                    <td class="col-25p no">LOCALIDAD:</td>
+                    <td class="col-25p">'. $localidad .'</td>
+                </tr>
+                <tr>
+                    <td class="col-25p no">Property_id:</td>
+                    <td class="col-25p">'.$propertyId .'</td>
+                    <td class="col-25p no">Indoor/Outdoor:</td>
+                    <td class="col-25p">'.$indoor_outdoor.'</td>
+                 </tr>
+                <tr>
+                    <td class="col-25p no">Código activo fijo:</td>
+                    <td class="col-25p">'.$cod_fijo.'</td>
+                    <td class="col-25p no">Código activo de energía:</td>
+                    <td class="col-25p">'.$cod_activo.'</td>
+                </tr>
+            </tbody>        
+        </table>									                        
+    </main>
+   
+  
+    ';
+    return $plantilla;
+}
 
 
 
---
+++ modulos/seguimiento_tecnico/catastro003/plantilla.php
@@ -0,0 +1,164 @@
+<?php
+require("../../../funciones/DateUtils.php");
+require("../ReporteCatastroUtils.php");
+
+
+function getPlantilla($conexion, $jsonData, $idgrupo, $idevento){
+
+    $obj = json_decode($jsonData);
+   // $check   = "<img style='vertical-align:middle' src='../../../img/checked.png'>";
+   // $uncheck = "<img style='vertical-align:middle' src='../../../img/unchecked.png'>";
+
+    $cabecera        = getCabecera003($conexion, $jsonData, $idgrupo, $idevento);
+
+
+    /** desarrollo_d **/
+    $desarrollo_d = $obj->{'desarrollo_d'};
+    $d01_01 = $desarrollo_d->d01_01;
+    $d02_01 = $desarrollo_d->d02_01;
+    $d03_01 = $desarrollo_d->d03_01;
+    $d04_01 = $desarrollo_d->d04_01;
+    $d05_01 = $desarrollo_d->d05_01;
+    $d06_01 = $desarrollo_d->d06_01;
+    $d07_01 = $desarrollo_d->d07_01;
+    $d08_01 = $desarrollo_d->d08_01;
+    $d09_01 = $desarrollo_d->d09_01;
+    $d10_01 = $desarrollo_d->d10_01;
+    $d11_01 = $desarrollo_d->d11_01;
+    $d12_01 = $desarrollo_d->d12_01;
+    $d13_01 = $desarrollo_d->d13_01;
+    $d14_01 = $desarrollo_d->d14_01;
+    $d15_01 = $desarrollo_d->d15_01;
+    $d16_01 = $desarrollo_d->d16_01;
+    $d17_01 = $desarrollo_d->d17_01; $d17_02 = $desarrollo_d->d17_02;
+    $d18_01 = $desarrollo_d->d18_01; $d18_02 = $desarrollo_d->d18_02;
+    $d19_01 = $desarrollo_d->d19_01;
+    $d20_01 = $desarrollo_d->d20_01;
+    $d21_01 = $desarrollo_d->d21_01;
+
+    $observaciones = $obj->{'observaciones'};
+    $plantilla =
+'<body>
+    <div class="card-">
+        <div class="card-body-">
+            <div id="invoice">
+                <div class="invoice">
+                
+                '. $cabecera .'
+                <main>
+                    <table class="tborder">
+                        <tbody>
+                           <tr>
+                                <td class="col-50p">Altura de operación - msnm</td>
+                                <td class="col-25p" colspan="2">'.$d01_01.'</td>
+                           </tr>
+                            <tr>
+                                <td class="col-50p">Funcionalidad (en servicio/fuera de servicio)</td>
+                                <td class="col-25p" colspan="2">'.$d02_01.'</td>
+                            </tr>
+                            <tr>
+                                <td class="col-50p">Estado (obsoleto/falta de capacidad/con falla)</td>
+                                <td class="col-25p" colspan="2">'.$d03_01.'</td>
+                           </tr>
+                            <tr>
+                                <td class="col-50p">Marca</td>
+                                <td class="col-25p" colspan="2">'.$d04_01.'</td>
+                           </tr>
+                            <tr>
+                                <td class="col-50p">Modelo</td>
+                                <td class="col-25p" colspan="2">'.$d05_01.'</td>
+                           </tr>
+                            <tr>
+                                <td class="col-50p">N° Serie</td>
+                                <td class="col-25p" colspan="2">'.$d06_01.'</td>
+                           </tr>
+                           <tr>
+                                <td class="col-50p">Tipo de uso (Indoor/Outdoor)</td>
+                                <td class="col-25p" colspan="2">'.$d07_01.'</td>
+                                
+                           </tr>
+                            <tr>
+                                <td class="col-50p">Cabina de insonorización</td>
+                                <td class="col-25p" colspan="2">'.$d08_01.'</td>
+                           </tr>
+                            <tr>
+                                <td class="col-50p">Sistema en baja tensión BT (Monofásico/trifásico)</td>
+                                <td class="col-25p" colspan="2">'.$d09_01.'</td>
+                           </tr>
+                            <tr>
+                                <td class="col-50p">Voltaje - V</td>
+                                <td class="col-25p" colspan="2">'.$d10_01.'</td>
+                           </tr>
+                           <tr>
+                                    <td class="col-50p">Frecuencia - Hz</td>
+                                <td class="col-25p" colspan="2">'.$d11_01.'</td>
+                           </tr>
+                           <tr>
+                                <td class="col-50p">Capacidad nominal - KVA</td>
+                                <td class="col-25p" colspan="2">'.$d12_01.'</td>
+                           </tr>
+                           <tr>
+                                <td class="col-50p">Capacidad efectiva - KVA</td>
+                                <td class="col-25p" colspan="2">'.$d13_01.'</td>
+                           </tr>
+                           <tr>
+                                <td class="col-50p">Tablero de transferencia ATS (Integrado/Externo)</td>
+                                <td class="col-25p" colspan="2">'.$d14_01.'</td>
+                           </tr>
+                          <tr>
+                                <td class="col-50p">Capacidad del tanque  integrado - Lt</td>
+                                <td class="col-25p" colspan="2">'.$d15_01.'</td>
+                          </tr>
+                          <tr>
+                                <td class="col-50p">Capacidad del tanque externo - Lt</td>
+                                <td class="col-25p" colspan="2">'.$d16_01.'</td>
+                          </tr>
+                          <tr>
+                                <td class="col-50p">bomba de trasiego / Automático - Manual</td>
+                                <td class="col-25p">'.$d17_01.'</td>
+                                <td class="col-25p">'.$d17_02.'</td>
+                          </tr>
+                          <tr>
+                                <td class="col-50p">Precalentador / Potencia, capacidad térmica - KW</td>
+                                <td class="col-25p">'.$d18_01.'</td>
+                                <td class="col-25p">'.$d18_02.'</td>
+                          </tr>
+                          <tr>
+                                <td class="col-50p">Fecha de inicio de operación</td>
+                                <td class="col-25p" colspan="2">'.$d19_01.'</td>
+                           </tr>
+                          <tr>
+                                <td class="col-50p">Proyecto</td>
+                                <td class="col-25p" colspan="2">'.$d20_01.'</td>
+                          </tr>
+                          <tr>
+                                <td class="col-50p">Fecha de vencimiento de garantía</td>
+                                <td class="col-25p" colspan="2">'.$d21_01.'</td>
+                          </tr>
+                         </tbody>        
+                    </table>									                        
+               </main>
+               <main>
+                    <table class="tborder">
+                        <tbody>
+                            <tr>
+                                 <td class="col-100p no"><strong>Observaciones</strong></td>
+                            </tr>
+                            
+                            <tr>
+                               <td>'.$observaciones.'</td>
+                           </tr>
+                        </tbody>
+                    </table>
+                </main>
+                </div>
+            </div>
+        </div>
+	</div>
+</body>
+';
+
+    return $plantilla;
+}
+
+?>
