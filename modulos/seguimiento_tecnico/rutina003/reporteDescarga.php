e912bc05f20ef791a129cfab504de25eb8d3f329
Ariel Siles <ariel.siles@gmail.com>
2024-03-15 00:14:42 -0400

    Descarga Rutina 03

---
+++ modulos/seguimiento_tecnico/rutina003/reporteDescarga.php
@@ -0,0 +1,36 @@
+<?php
+require("../../../funciones/motor.php");
+require("../DataMtoUtils.php");
+require_once('../../../vendor/autoload.php');
+require_once('plantilla.php');
+
+$idrutinax = $_POST['idrutinax'];
+$cform = $_POST['cform'];
+$idgrupo = $_POST['idgrupo'];
+$iddepartamento = $_POST['iddepartamento'];
+
+$css = file_get_contents('../../../assets/css/report.css');
+
+$datosArr     = getJsonData_rutinaArr($conexion, $idrutinax, $cform);
+$idformulario = $datosArr[0];
+$jsonData     = $datosArr[1];
+$titulosArr   = getTitulosRutinas($conexion, $idformulario);
+$plantilla    = getPlantilla($conexion, $jsonData, $idgrupo, $idrutinax, $titulosArr);
+$nombreFile   = getNombreArchivoReporte($jsonData, $titulosArr[0]);
+$nombreFile   = getNombreArchivoReporte($jsonData, $titulosArr[0]);
+$nombreFile   = $nombreFile.".pdf";
+
+$mpdf = new \Mpdf\Mpdf(['format' => 'Letter']);
+$mpdf->AddPage('P', '', '', '', '', '3', '3', '1.5', '1.5');
+
+$mpdf->writeHtml($css, \Mpdf\HTMLParserMode::HEADER_CSS);
+$mpdf->writeHtml($plantilla, \Mpdf\HTMLParserMode::HTML_BODY);
+$pdfContent = $mpdf->output($nombreFile, \Mpdf\Output\Destination::STRING_RETURN);
+
+header('Content-Type: application/pdf');
+header('Content-Disposition: attachment; filename="' . $nombreFile . '"');
+header('Content-Length: ' . strlen($pdfContent));
+
+echo $pdfContent;
+
+?>
